e6482b305a15b500edc82c7ae87a9e4edbe758f6
Michael Goncalves <michaeldev.sjb@gmail.com>
2024-11-12 20:27:06 -0300

    GameList

---
+++ src/main/java/com/michaeldevsjb/dslist/projections/GameMinProjection.java
@@ -0,0 +1,17 @@
+package com.michaeldevsjb.dslist.projections;
+
+public interface GameMinProjection {
+
+    Long getId();
+
+    String getTitle();
+
+    Integer getYear();
+
+    String getImgUrl();
+
+    String getShortDescription();
+
+    Integer getPosition();
+
+}
--- src/main/java/com/michaeldevsjb/dslist/services/GameService.java
+++ src/main/java/com/michaeldevsjb/dslist/services/GameService.java
@@ -9,6 +9,7 @@ import org.springframework.transaction.annotation.Transactional;
 import com.michaeldevsjb.dslist.dto.GameDTO;
 import com.michaeldevsjb.dslist.dto.GameMinDTO;
 import com.michaeldevsjb.dslist.entities.Game;
+import com.michaeldevsjb.dslist.projections.GameMinProjection;
 import com.michaeldevsjb.dslist.repositories.GameRepository;
 
 @Service
@@ -27,6 +28,12 @@ public class GameService {
     public List<GameMinDTO> findAll() {
         List<Game> result = gameRepository.findAll();
         return result.stream().map(x -> new GameMinDTO(x)).toList();
-
     }
+
+    @Transactional(readOnly = true)
+    public List<GameMinDTO> findByList(Long listId) {
+        List<GameMinProjection> result = gameRepository.searchByList(listId);
+        return result.stream().map(x -> new GameMinDTO(x)).toList();
+    }
+
 }
